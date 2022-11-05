87b6336ada7b47027cda0e50daf91002a52e347a
Dmitriy Mashurov <lagrik33@gmail.com>
2022-11-06 00:50:29 +0300

    Добавлено дто для подачи заявления

---
+++ src/main/java/ru/mashurov/rest/model/dto/AppointmentRequestCreateDto.java
@@ -0,0 +1,23 @@
+package ru.mashurov.rest.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentRequestCreateDto {
+
+    private Long veterinarianId;
+
+    private Long clinicId;
+
+    private String appointmentPlace;
+
+    private Long petId;
+
+    private Long serviceId;
+}

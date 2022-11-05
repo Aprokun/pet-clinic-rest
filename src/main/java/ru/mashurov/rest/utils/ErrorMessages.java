11603c1495af8f0d030c79df2f99e60d3bb5323c
Dmitriy Mashurov <lagrik33@gmail.com>
2022-11-06 00:50:55 +0300

    Добавлены вспомогательные классы

---
+++ src/main/java/ru/mashurov/rest/utils/ErrorMessages.java
@@ -0,0 +1,10 @@
+package ru.mashurov.rest.utils;
+
+public class ErrorMessages {
+
+    public static final String USER_NOT_REGISTERED = "Пользователь не зарегистрирован!";
+    public static final String PET_NOT_EXIST = "Данного животного не существует или оно не принадлежит вам!";
+    public static final String CLINIC_NOT_EXIST = "Данной клиники не существует или она не работает!";
+    public static final String SERVICE_NOT_EXIST = "Данной услуги не существует!";
+    public static final String VETERINARIAN_NOT_EXIST = "Данного ветеринара не существует или он больше не работает в данной клинике";
+}

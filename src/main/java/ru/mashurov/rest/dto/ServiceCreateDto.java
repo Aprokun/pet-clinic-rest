7220d090db3d9eaf5db1867770c65b41fb9029cc
Dmitriy <lagrik33@gmail.com>
2022-12-20 23:16:18 +0300

    Добавления/изменения функционала для услуг

---
+++ src/main/java/ru/mashurov/rest/dto/ServiceCreateDto.java
@@ -0,0 +1,15 @@
+package ru.mashurov.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class ServiceCreateDto extends ServiceDto {
+
+	private Long clinicId;
+}
--- src/main/java/ru/mashurov/rest/services/ServiceService.java
+++ src/main/java/ru/mashurov/rest/services/ServiceService.java
@@ -3,12 +3,12 @@ package ru.mashurov.rest.services;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
+import ru.mashurov.rest.model.Service;
 import ru.mashurov.rest.repositories.ServiceRepo;
 
 import static ru.mashurov.rest.utils.ErrorMessages.SERVICE_NOT_EXIST;
 
-@Service
+@org.springframework.stereotype.Service
 @AllArgsConstructor
 public class ServiceService {
 
@@ -18,14 +18,22 @@ public class ServiceService {
 
         if (!serviceRepo.existsById(id)) {
 
-            //TODO 404
-            throw new RuntimeException(SERVICE_NOT_EXIST);
+	        //TODO 404
+	        throw new RuntimeException(SERVICE_NOT_EXIST);
         }
 
-        return serviceRepo.findById(id).get();
+	    return serviceRepo.findById(id).get();
     }
 
-    public Page<ru.mashurov.rest.model.Service> findAllByClinicId(final Long clinicId, final Pageable pageable) {
-        return serviceRepo.findAllByClinicId(clinicId, pageable);
-    }
+	public Page<ru.mashurov.rest.model.Service> findAllByClinicId(final Long clinicId, final Pageable pageable) {
+		return serviceRepo.findAllByClinicId(clinicId, pageable);
+	}
+
+	public void save(final Service service) {
+		serviceRepo.save(service);
+	}
+
+	public void deleteById(final Long id) {
+		serviceRepo.deleteById(id);
+	}
 }
